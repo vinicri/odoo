fa9dcb270df177725034c8cdda2537131e84cacd
Vinicius <vinicri@gmail.com>
2026-06-14 18:46:22 -0300

    l10n_br_nfe uom nfe_name to be unique among uoms

--- hiperpan/addons/l10n_br_nfe/models/uom_uom.py
+++ hiperpan/addons/l10n_br_nfe/models/uom_uom.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class UoM(models.Model):
@@ -7,3 +8,33 @@ class UoM(models.Model):
     nfe_name = fields.Char(
         string="Nome na NFE/NFCE", size=6, help="Name of the UOM for NFE"
     )
+
+    _sql_constraints = [
+        (
+            "nfe_name_uniq",
+            "unique(nfe_name)",
+            "O Nome na NFE/NFCE deve ser único entre as unidades de medida.",
+        ),
+    ]
+
+    @api.constrains("nfe_name")
+    def _check_nfe_name_unique(self):
+        for uom in self:
+            if not uom.nfe_name:
+                continue
+            duplicate = self.search(
+                [
+                    ("nfe_name", "=", uom.nfe_name),
+                    ("id", "!=", uom.id),
+                ],
+                limit=1,
+            )
+            if duplicate:
+                raise ValidationError(
+                    _(
+                        "O Nome na NFE/NFCE '%(nfe_name)s' já está em uso pela "
+                        "unidade de medida '%(uom_name)s'. Escolha um valor único.",
+                        nfe_name=uom.nfe_name,
+                        uom_name=duplicate.name,
+                    )
+                )
